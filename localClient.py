4984dc505b142fab74829103f8e8a6defc8629e5
Owen Busler <owen1050@gmail.com>
2020-03-23 12:11:49 -0400

    Owen: local works for mouse and letters and backspace

--- localClient.py
+++ localClient.py
@@ -1,9 +1,9 @@
 import requests, pyautogui, base64, io, time
 
-url = "http://127.0.0.1:23655"
+url = "http://192.168.1.196:23655"
 t0 = 0
 while(True):
-	if(time.time() - t0 >1):
+	if(True): #time.time() - t0 >1):
 		t01 = time.time()
 		ss = pyautogui.screenshot()
 
@@ -16,3 +16,37 @@ while(True):
 		print(len(send))
 		print(time.time() - t01)
 		t0 = time.time()
+
+
+		r = requests.get(url,data =  {"cp:~".encode()})	
+		try:
+			x = int(r.text[0:r.text.find(",")])
+			y = int(r.text[r.text.find(",")+1:r.text.find("~")])
+			print(x,y,"MOVE")
+			pyautogui.moveTo(x,y)
+		except:
+			pass
+
+		r = requests.get(url,data =  {"cc:~".encode()})	
+		try:
+			x = int(r.text[0:r.text.find(",")])
+			y = int(r.text[r.text.find(",")+1:r.text.find("~")])
+			print(x,y, "CLICK")
+			pyautogui.click(x = x,y = y)
+		except:
+			pass
+
+		r = requests.get(url,data =  {"kb:~".encode()})
+		data = r.text
+		print(data)
+		try:
+			while(len(data) > 1):
+				k = data[0:data.find(",")]
+				if(k == "BackSpace"):
+					pyautogui.press("backspace")
+				else:
+					pyautogui.press(k)
+				print("pressed" + str(k))
+				data = data[data.find(",")+1:]
+		except:
+			pass
