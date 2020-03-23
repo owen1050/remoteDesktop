458f55b503d9354154de647881afbdab3ca64f4b
owen1050 <owen1050@gmail.com>
2020-03-23 03:44:36 -0400

    Owen: horid performace but remote and local can transmit the screen

--- localClient.py
+++ localClient.py
@@ -1,15 +1,7 @@
 import requests, pyautogui, base64, io, time
 
 url = "http://127.0.0.1:23655"
-ss = pyautogui.screenshot()
 
-buffer = io.BytesIO()
-ss.save(buffer, format = "JPEG")
-ss = buffer.getvalue()
-b64string = base64.b64encode(ss)
-send = "i:" + str(b64string)[2:-1] + "~"
-print(len(send))
-t0 = time.time()
 while(True):
 	if(True):
 		t01 = time.time()
--- remoteClient.py
+++ remoteClient.py
@@ -1,13 +1,47 @@
-import requests, pyautogui, base64, io, time
+import requests, pyautogui, base64, time
+from tkinter import *
+from io import BytesIO
+from PIL import Image, ImageTk
+
+data = ""
+render = ""
+img = ""
+class Window(Frame):
+    def __init__(self, master=None):
+        global data, render, img
+        Frame.__init__(self, master)
+        self.master = master
+        self.pack(fill=BOTH, expand=1)
+
+        
+
+        im = Image.open(BytesIO(base64.b64decode(data)))
+        
+        render = ImageTk.PhotoImage(im)
+        img = Label(self, image=render)
+        img.image = render
+        img.place(x=0, y=0)
+
+def updateImg():
+    global data, render, root,img
+    url = "http://127.0.0.1:23655"
+    r = requests.get(url, data =  {"i:~".encode()})
+    data = r.text
+    im = Image.open(BytesIO(base64.b64decode(data)))        
+    render = ImageTk.PhotoImage(im)
+    img.config(image = render)
+    root.after(250,updateImg)
+    print("RAN")
 
 url = "http://127.0.0.1:23655"
+r = requests.get(url, data =  {"i:~".encode()})
+data = r.text
 
-t0 = time.time()
-for i in range(18):
-	r = requests.get(url, data =  {"i:~".encode()})
-t1 = time.time()
-print(t1-t0)
+root = Tk()
 
-f = open("output.png", "wb")
-f.write(io.BytesIO(base64.b64decode(r.text)).read())
-f.close()
+app = Window(root)
+root.after(1000,updateImg)
+root.wm_title("Tkinter window")
+root.geometry("200x120")
+root.mainloop()
+
